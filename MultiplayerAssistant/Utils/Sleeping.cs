bfa59ef3f01825fde32fdf8471497224a1106d17
sycho <vbdd@msn.com>
2025-09-19 21:01:17 +0800

    feat(modentry): enhance mod functionality with new features
    - Introduce a `WaitCondition` class to manage state before mod execution
    - Add infinite health and stamina for the host when the world is ready
    - Implement a check for the title menu before enabling the farm stage
    - Integrate `ModConfig` and new event handling on game update
    - Improve mod structure and add several TODOs for future enhancements

---
+++ MultiplayerAssistant/Utils/Sleeping.cs
@@ -0,0 +1,26 @@
+﻿using MultiplayerAssistant.HostAutomatorStages;
+using Netcode;
+using StardewValley;
+using StardewValley.Network;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MultiplayerAssistant.Utils
+{
+    internal class Sleeping
+    {
+        public static bool IsSleeping()
+        {
+            return ReadyCheckHelper.IsReady("sleep", Game1.player);
+        }
+        public static bool OthersInBed(int numOtherPlayers)
+        {
+            return Game1.player.team.GetNumberReady("sleep") == (numOtherPlayers + (IsSleeping() ? 1 : 0));
+        }
+        public static bool ShouldSleep(int numOtherPlayers)
+        {
+            return numOtherPlayers > 0 && (Game1.timeOfDay >= 2530 || OthersInBed(numOtherPlayers));
+        }
+    }
+}
